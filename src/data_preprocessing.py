bb711f0c3b43f982a5a928cd1a544df29c060504
Your Name <you@example.com>
2024-09-23 13:50:00 +0530

    Preprocess first

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,9 +4,18 @@ import pandas as pd
 dataset = pd.read_csv("/Users/hemantsekhri/Documents/IML/fuel_train.csv")
 
 dataset.columns = dataset.columns.str.strip()  # Removes leading and trailing spaces in titles
-dataset.isnull().sum() # Check for missing values
-dataset.duplicated().sum() # Check for duplicate values
+#dataset.isnull().sum() # Check for missing values
+#dataset.duplicated().sum() # Check for duplicate values
 
 dataset.dropna(inplace=True)
 dataset.drop_duplicates(inplace=True)
 
+#dataset.corr()['FUEL CONSUMPTION'] # To find correlation
+
+X = dataset[['ENGINE SIZE','CYLINDERS','COEMISSIONS']] # Features (independent)
+Y = dataset['FUEL CONSUMPTION'] # Target (dependent)
+X = np.c_[np.ones(X.shape[0]), X] # Adding a column of 1s for constant in matrix multiplication
+
+split_size = int(0.75*len(dataset)) # Split X and Y into training and testing set in 8:2
+X_train, Y_train = [X[:split_size]], [Y[:split_size]]
+X_test, Y_test = [X[split_size:]], [Y[split_size:]]
--- src/train_model.py
+++ src/train_model.py
@@ -1,2 +1,6 @@
+import numpy as np 
+import pandas as pd 
+import data_preprocessing
 
+#Equation of line = w0 + w1x1 + w2x2 + w3x3 
 
