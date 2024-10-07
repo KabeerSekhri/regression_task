9df7f5870e8317d884bb68a11fcd957c8d83991a
Your Name <you@example.com>
2024-10-07 11:16:09 +0530

    done, i think?

--- src/predict.py
+++ src/predict.py
@@ -9,7 +9,6 @@ n= 1000
 L = 0.00001
 
 # Basic model without vetorization
-'''
 cost_list = []
 W = np.random.randint(0,1,size=(X.shape[1],1)).astype(np.float64) # Initialise W to 0s
 
@@ -26,7 +25,6 @@ with open('../models/regression_model1.pkl', 'rb') as file:
     loaded_w = pickle.load(file)
 
 y_pred_b = np.dot(X, loaded_w) 
-'''
 
 
 # Faster model with vectorization (matrix)
@@ -39,6 +37,16 @@ with open('../models/regression_model_final.pkl', 'wb') as file:
 with open('../models/regression_model_final.pkl', 'rb') as file:
     loaded_w = pickle.load(file)
 
-y_pred_v = np.dot(X, loaded_w) 
-y_pred_v = pd.DataFrame(y_pred_v)
-y_pred_v.to_csv('../results/train_predictions.csv', index=False, header=False) 
+y_pred_v = np.dot(X, loaded_w) # Predict Y using pickle file
+
+def metrics(y,cost):
+    with open('../results/train_metrics.txt','w') as  file:
+        file.write(f"Regression Metrics\n")
+        file.write(f"Mean Squared Error (MSE): {cost:.2}\n")
+        file.write(f"Root Mean Squared Error (RMSE): {np.sqrt(cost):.2}\n")
+        file.write(f"R-squared (R²) Score: {1- ((np.sum((Y - y) ** 2))/np.sum((Y- np.mean(Y))**2)):.2}")
+    
+    y = pd.DataFrame(y) # Conver to pandas Data Frame
+    y.to_csv('../results/train_predictions.csv', index=False, header=False) # Output data frame as csv to another file
+
+metrics(y_pred_v,final_cost)
